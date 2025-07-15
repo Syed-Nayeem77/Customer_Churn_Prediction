ec868031096fa6af42d8eeccd3a69c181d388148
Syed Abdul Nayeem <137449356+Syed-Nayeem77@users.noreply.github.com>
2025-07-16 01:23:21 +0530

    Create schemas.py

---
+++ src/app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class CustomerData(BaseModel):
+    customerID: str
+    gender: str
+    SeniorCitizen: int
+    Partner: str
+    Dependents: str
+    tenure: int
+    PhoneService: str
+    # Add all other features from your dataset
